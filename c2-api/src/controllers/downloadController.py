8f40d4cc8348b16cdb664b975b196746e24edd40
Arun Govindasamy <govindasamyarun@gmail.com>
2024-01-18 23:48:55 +0530

    exploit upload/download bug fix

--- c2-api/src/controllers/downloadController.py
+++ c2-api/src/controllers/downloadController.py
@@ -11,9 +11,9 @@ logger_instance = init_logger('C2_Cloud')
 
 def download(encoded_filename):
     logger_instance.info(f"download - encoded_filename: {encoded_filename}")
-    filename = base64.b64decode(encoded_filename).decode('utf-8')
-    filepath = "/usr/src/app/src/exploits/" + filename
-    if os.path.exists(filename) and os.path.isfile(filename):
+    #filename = base64.b64decode(encoded_filename).decode('utf-8')
+    filepath = "/usr/src/app/src/exploits/" + encoded_filename
+    if os.path.exists(filepath) and os.path.isfile(filepath):
         return send_file(filepath, as_attachment=True)
     else:
         return "File not found or invalid file path"
